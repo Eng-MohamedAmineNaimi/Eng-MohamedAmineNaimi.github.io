2c36bfd18859a8fdd559481a7bbb56da5ad2e2b3
Eng-MohamedAmineNaimi <mohamedaminenaimi43@gmail.com>
2025-02-03 01:07:19 +0000

    securiser le repo priveééé

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/",
-  
+  base: "/portfolio/",  
   build: {
+    outDir: "dist", 
     rollupOptions: {
       output: {
         manualChunks: {
