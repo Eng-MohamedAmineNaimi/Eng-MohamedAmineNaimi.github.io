d97159baaa964f0aee0038e37f89aaa639823951
Eng-MohamedAmineNaimi <mohamedaminenaimi43@gmail.com>
2025-02-11 01:44:21 +0000

    go go go

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import viteImagemin from 'vite-plugin-imagemin'; 
 
 export default defineConfig({
-  plugins: [
-    react(),
-    viteImagemin({
-      gifsicle: {
-        optimizationLevel: 3, 
-      },
-      optipng: {
-        optimizationLevel: 7, 
-      },
-      pngquant: {
-        quality: [0.6, 0.8], 
-      },
-      svgo: {
-        plugins: [
-          {
-            removeViewBox: false, 
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
   base: "/portfolio/",  
   build: {
     outDir: "dist", 
